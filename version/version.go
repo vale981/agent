a9979183725a2eef9c6f4e99ae4fdf255b2d82b0
dencoded <33698537+dencoded@users.noreply.github.com>
2020-04-08 00:15:46 -0400

    bump version to 1.0.6

--- version/version.go
+++ version/version.go
@@ -6,7 +6,7 @@ import (
 	"github.com/fatih/color"
 )
 
-var AgentVersion = "1.0.5"
+var AgentVersion = "1.0.6"
 
 func CheckAgentVersion(latestVer string) {
 	log.Println("Current agent version:", AgentVersion)
